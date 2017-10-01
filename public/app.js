b04a3be9e247d3f68e8847efe4ebd3ea4bc7ad9d
mariya-ivanova <maria_m11m@yahoo.com>
2017-10-01 17:10:32 +0300

    added banners page, changed styles in websites and home page

--- public/app.js
+++ public/app.js
@@ -2,11 +2,12 @@
 import Sammy from 'sammy'
 import 'jquery' 
 import templates from 'templates.js' 
-import aboutmeController from 'controllers/aboutmeController.js'
 import homeController from 'controllers/homeController.js'
+import aboutmeController from 'controllers/aboutmeController.js'
 import portfolioController from 'controllers/portfolioController.js'
 import websitesController from 'controllers/websitesController.js'
 import paintingsController from 'controllers/paintingsController.js'
+import bannersController from 'controllers/bannersController.js'
 
 var containerId = '#main';
 var sammyApp = Sammy(containerId, function() {
@@ -15,10 +16,11 @@ var sammyApp = Sammy(containerId, function() {
     });
 
 	this.get('#/home', homeController.all);		
+    this.get('#/aboutme', aboutmeController.all);	
 	this.get('#/portfolio', portfolioController.all);	
 	this.get('#/websites', websitesController.all);	
 	this.get('#/paintings', paintingsController.all);		
-    this.get('#/aboutme', aboutmeController.all);	
+    this.get('#/banners', bannersController.all);	
 
     templates.load('main-navbar')
 		.then(function(template) {				
