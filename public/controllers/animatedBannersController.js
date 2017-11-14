5ea50ae23d561427fc7def87befc5e502aafd646
mariya-ivanova <maria_m11m@yahoo.com>
2017-11-14 17:16:57 +0200

    small fix

--- public/controllers/animatedBannersController.js
+++ public/controllers/animatedBannersController.js
@@ -1,7 +1,7 @@
 // animatedBannersController
 import 'jquery'
 import templates from 'templates.js'
-import myBanner from 'myBanner.js'
+import mybanner from 'mybanner.js'
 
 export default {
    all: function() {
@@ -9,7 +9,7 @@ export default {
         templates.load('animatedBanners')
             .then(function(data) {								
 				$('#main').html(data);				
-				myBanner.all();				
+				mybanner.all();				
         });			
     }
 }
