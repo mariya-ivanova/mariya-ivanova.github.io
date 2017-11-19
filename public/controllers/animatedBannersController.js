8045567bb975a8c9314085539659eb2bae9f540d
mariya-ivanova <maria_m11m@yahoo.com>
2017-11-19 15:43:50 +0200

    added new size of the animated banner

--- public/controllers/animatedBannersController.js
+++ public/controllers/animatedBannersController.js
@@ -2,14 +2,15 @@
 import 'jquery'
 import templates from 'templates.js'
 import mybanner from 'mybanner.js'
+import mybanner250x400 from 'mybanner250x400.js'
 
 export default {
    all: function() {
-
         templates.load('animatedBanners')
             .then(function(data) {								
-				$('#main').html(data);				
-				mybanner.all();				
-        });			
+				$('#main').html(data);			
+				mybanner.all();	
+				mybanner250x400.all();		
+        });		
     }
 }
