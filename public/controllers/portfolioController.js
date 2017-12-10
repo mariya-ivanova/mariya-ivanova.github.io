70d55524ff8e41b322f6f372d5fe67c240b2b312
mariya-ivanova <maria_m11m@yahoo.com>
2017-12-10 16:11:49 +0200

    small changes in html content and style

--- public/controllers/portfolioController.js
+++ public/controllers/portfolioController.js
@@ -3,16 +3,26 @@ import 'jquery'
 import templates from 'templates.js'
 
 export default {
-   all: function() {
+   all: function(sammy) {
 
         templates.load('languagebar')
         .then(function(data) {                          
             $('#language-bar').html(data('portfolio'));   
         });     	
 
-        templates.load('portfolio')
-            .then(function(data) {								
-				$('#main').html(data);									
-        });			
+        var lang = sammy.params['lang'] || ':bg';
+        if(lang == ':en') {
+            templates.load('portfolioen')
+            .then(function(data) {                          
+                $('#main').html(data);  
+            });         
+        }
+        else {
+            templates.load('portfolio')
+            .then(function(data) {                          
+                $('#main').html(data);                                              
+            }); 
+        }   
+
     }
 }
